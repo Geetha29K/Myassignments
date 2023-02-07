3638da502518fd15f8fdef0fb1ef314187adaec8
Geetha29K <124452189+Geetha29K@users.noreply.github.com>
2023-02-07 14:24:53 +0530

    Add files via upload
    Print Oddnumbers

---
+++ day2/LearnIfStatement.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class LearnIfStatement {
+
+public static void main(String[] args) {
+	int a =21;
+	if(a>=18)
+{
+		System.out.println("you are eligible");
+		}
+}
+}
---
+++ day2/PrintOddNumbers.java
@@ -0,0 +1,10 @@
+package week1.day2;
+
+public class PrintOddNumbers {
+public static void main(String[] args) {
+	for(int i=1;i<=20;i=i+1)
+		if(i % 2 == 1) {
+			System.out.println(i);
+		}
+}
+}
